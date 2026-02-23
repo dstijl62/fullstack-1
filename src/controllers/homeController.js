caacb8d622775885174b95aa389821d8dcf74621
dstijl62 <dstijl62@gmail.com>
2026-02-23 21:40:27 +0700

    Create a new user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,24 +1,36 @@
+import db from "../models/index";
 
-import db from '../models/index';
+import CRUDService from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
-    try {
-           let data = await db.User.findAll();
+  try {
+    let data = await db.User.findAll();
 
-           return res.render('homepage.ejs', {
-            data: JSON.stringify(data)
-           });
-
-    } catch(e) {
-        console.log(e)
-    }
-}
-
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 let getAboutPage = (req, res) => {
-    return res.render('about.ejs');
-}
+  return res.render("about.ejs");
+};
+
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  return res.send("create a new user succeed");
+};
 
 module.exports = {
-    getHomePage, getAboutPage
-}
+  getHomePage,
+  getAboutPage,
+  getCRUD,
+  postCRUD,
+};
