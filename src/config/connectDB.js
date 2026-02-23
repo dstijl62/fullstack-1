113077423ba41dcee877521edcb6af410624245a
dstijl62 <dstijl62@gmail.com>
2026-02-23 16:51:10 +0700

    create database table

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,21 +1,20 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 // Option2: Passing parameters separately (other dialects)
 // const sequelize = new Sequelize('database', 'username', 'password'
-const sequelize = new Sequelize('test1', 'root', null, {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false
-}); 
+const sequelize = new Sequelize("bookingcare", "root", null, {
+  host: "localhost",
+  dialect: "mysql",
+  logging: false,
+});
 
-
-let connectDB = async(req, res) => {
-    try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
-}
+let connectDB = async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 
 module.exports = connectDB;
